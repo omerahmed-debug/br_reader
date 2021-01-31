a542ec4dc9d856bd8b00424b7e39ccffe4068f22
Sundus Yousuf <sundus.ahmed.yousuf@gmail.com>
2021-01-31 10:24:26 +0300

    Add date picker for history

--- br_reader_app/views.py
+++ br_reader_app/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from br_reader_app.models import Reading
 from br_reader_app.capture import Capture
+from django.utils.timezone import make_aware
+from django.conf import settings
+import datetime
+
 # Create your views here.
 def index(request):
     readings = Reading.objects.all().order_by('-read_at')[:48]
@@ -13,5 +17,13 @@ def read(request):
     return HttpResponseRedirect('/')
 
 def history(request):
-    readings = Reading.objects.all().order_by('-read_at')
-    return render(request, 'index.html', {'readings': readings, 'table_label': 'Showing All History Readings'})
+    if 'date_picker' in request.GET:
+        selectedDate = request.GET['date_picker']
+    else:
+        selectedDate = datetime.date.today().strftime('%Y-%m-%d')
+    st = datetime.datetime.strptime(selectedDate, '%Y-%m-%d')
+    ed = datetime.datetime.strptime(selectedDate, '%Y-%m-%d').replace(hour=23, minute=59, second=59)
+    st = make_aware(st)
+    ed = make_aware(ed)
+    readings = Reading.objects.filter(read_at__range=[st, ed]).order_by('-read_at')
+    return render(request, 'index.html', {'readings': readings, 'table_label': 'Showing All History Readings', 'date_picker': selectedDate})
